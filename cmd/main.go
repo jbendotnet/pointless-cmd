3a4ae61a37d105ba1329a51a5dcda15a562079c0
Jon Bennett <jon.bennett@futurenet.com>
2018-06-25 17:15:50 +0100

    hmm, v2 of three

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log"
+
+	"github.com/jbendotnet/pointless2/three"
 	// "github.com/jbendotnet/pointless/one"
 	// "github.com/jbendotnet/pointless/two"
 	"github.com/jbendotnet/pointless/v3/one"
@@ -8,10 +11,14 @@ import (
 
 	onev4 "github.com/jbendotnet/pointless/v4/one"
 	twov4 "github.com/jbendotnet/pointless/v4/two"
+
+	three "github.com/jbendotnet/pointless2/three/v2"
 )
 
 func main() {
 	one.Do(two.Get())
 	onev4.Do(twov4.Get())
 	// one.Do(twov3.Get())
+	ls := three.List()
+	log.Println(ls)
 }
--- go.mod
+++ go.mod
@@ -3,4 +3,5 @@ module github.com/jbendotnet/pointless-cmd
 require (
 	github.com/jbendotnet/pointless/v3 v3.0.0
 	github.com/jbendotnet/pointless/v4 v4.0.0
+	github.com/jbendotnet/pointless2/three/v2 v2.0.1
 )
