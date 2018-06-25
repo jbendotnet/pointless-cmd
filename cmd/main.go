00cefe09b2fc03030fbf01c7b5eecdc1bbc8d85d
Jon Bennett <jon.bennett@futurenet.com>
2018-06-25 16:52:13 +0100

    v4 test

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	// "github.com/jbendotnet/pointless/one"
 	// "github.com/jbendotnet/pointless/two"
-	onev3 "github.com/jbendotnet/pointless/v3/one"
-	twov3 "github.com/jbendotnet/pointless/v3/two"
+	"github.com/jbendotnet/pointless/v3/one"
+	"github.com/jbendotnet/pointless/v3/two"
+
+	onev4 "github.com/jbendotnet/pointless/v4/one"
+	twov4 "github.com/jbendotnet/pointless/v4/two"
 )
 
 func main() {
-	onev3.Do(twov3.Get())
+	one.Do(two.Get())
+	onev4.Do(twov4.Get())
 	// one.Do(twov3.Get())
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
 module github.com/jbendotnet/pointless-cmd
 
-require github.com/jbendotnet/pointless/v3 v3.0.0
+require (
+	github.com/jbendotnet/pointless/v3 v3.0.0
+	github.com/jbendotnet/pointless/v4 v4.0.0
+)
