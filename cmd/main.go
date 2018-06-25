3dc9cf0f15582ae01504c88d0a5386a959b0216f
Jon Bennett <jon.bennett@futurenet.com>
2018-06-25 17:17:38 +0100

    v2 of three please

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 
-	"github.com/jbendotnet/pointless2/three"
 	// "github.com/jbendotnet/pointless/one"
 	// "github.com/jbendotnet/pointless/two"
 	"github.com/jbendotnet/pointless/v3/one"
@@ -12,13 +11,13 @@ import (
 	onev4 "github.com/jbendotnet/pointless/v4/one"
 	twov4 "github.com/jbendotnet/pointless/v4/two"
 
-	three "github.com/jbendotnet/pointless2/three/v2"
+	threev2 "github.com/jbendotnet/pointless2/three/v2"
 )
 
 func main() {
 	one.Do(two.Get())
 	onev4.Do(twov4.Get())
 	// one.Do(twov3.Get())
-	ls := three.List()
+	ls := threev2.List()
 	log.Println(ls)
 }
