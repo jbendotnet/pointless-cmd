0fe391f7d0afe7b0b0f38f533838810d264fbae6
Jon Bennett <jon.bennett@futurenet.com>
2018-06-25 17:22:33 +0100

    commit

--- cmd/main.go
+++ cmd/main.go
@@ -3,21 +3,19 @@ package main
 import (
 	"log"
 
-	// "github.com/jbendotnet/pointless/one"
-	// "github.com/jbendotnet/pointless/two"
 	"github.com/jbendotnet/pointless/v3/one"
 	"github.com/jbendotnet/pointless/v3/two"
 
 	onev4 "github.com/jbendotnet/pointless/v4/one"
 	twov4 "github.com/jbendotnet/pointless/v4/two"
 
-	threev2 "github.com/jbendotnet/pointless2/three/v2"
+	three "github.com/jbendotnet/pointless2/three/v2"
 )
 
 func main() {
-	one.Do(two.Get())
-	onev4.Do(twov4.Get())
+	one.Do(twov4.Get())
+	onev4.Do(two.Get())
 	// one.Do(twov3.Get())
-	ls := threev2.List()
+	ls := three.List()
 	log.Println(ls)
 }
